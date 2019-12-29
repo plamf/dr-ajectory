9c248b9262e6fd93ec08c00f251b99af7ed5f82c
Franz Plam <franzl@live.at>
2019-12-29 21:23:10 +0100

    Implement velocity formula (#3)

--- main/mathLib.ino
+++ main/mathLib.ino
@@ -1,21 +1,31 @@
+const int iboRating = 300; //not sure if this should be constant or configurable
+int additionalWeightOnBowstringInGrams = 5;
+
 double convertForceIntoNewtons(int pounds){
   double newtonsPerPound = 4.448;
+  
   return pounds * newtonsPerPound;
 }
 
-double convertDrawLengthInToMeters(int centimeters){
-  double drawLengthFactor = 2.5;
-  return centimeters / drawLengthFactor / 100;
+double convertDrawLengthIntoMeters(int centimeters){
+  double drawlengthFactor = 2.5;
+  double centimetersPerInch = 2.54;
+  double inches = centimeters / centimetersPerInch / drawlengthFactor;
+  
+  return inches;
 }
 
 double convertProjectileMassIntoKilograms(int grains){
   double grainsPerKilogram = 15432;
+  
   return grains / grainsPerKilogram;
 }
 
-double calculateVelocity(double force, double drawLength, double mass){
-  //Todo: actual calculation
-  return 0;
+double calculateVelocity(double F, double L, double m, double R, double W){
+  double feetPerSecond = R + (L - 30) * 10 - W / 3 + min(0, -(m - 5 * F) / 3);
+  double metersPerSecond = feetPerSecond / 3.281;
+  
+  return metersPerSecond;
 }
 
 int getDistance(){
@@ -23,11 +33,11 @@ int getDistance(){
   return 50;
 }
 
-int calculateProjectileTrajectory(int distance, int arrowWeight, int drawLength, int force){
-  double F = convertForceIntoNewtons(force);
-  double L = convertDrawLengthInToMeters(drawLength);
-  double m = convertProjectileMassIntoKilograms(arrowWeight);
-  double v = calculateVelocity(F, L, m);
+int calculateProjectileTrajectory(double lbsOfForce, double cmDrawlength, double arrowWeightGrains){
+  double F = convertForceIntoNewtons(lbsOfForce);
+  double L = convertDrawLengthIntoMeters(cmDrawlength);
+  double m = convertProjectileMassIntoKilograms(arrowWeightGrains);
+  double v = calculateVelocity(F, L, m, iboRating, additionalWeightOnBowstringInGrams);
 
   //Todo: actual calculation
   return 45;
